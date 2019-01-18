3442721d4adcb4d2960fc81053b3b409c6dfb083
XxTheAlexGamerxX <45047557+XxTheAlexGamerxX@users.noreply.github.com>
2019-01-18 21:28:28 +0100

    Update celine.py

--- celine.py
+++ celine.py
@@ -1,15 +1,8 @@
 import discord
 from discord.ext import commands
-from tinydb import TinyDB, Query
-from tinydb.operations import delete
+
 import random
 import time
-
-db=TinyDB('data_Celine.json')
-user=Query()
-
-TOKEN='NTM1NTk2NzA1NzQ4NDg0MDk2.DyKdEg.4W9xOx4cqMfzXa_IjUv3tMVgnCg'
-
 client = commands.Bot(command_prefix = '+')
 client.remove_command('help')
 
@@ -34,4 +27,4 @@ async def on_member_join(member):
 	
 	await client.send_message(discord.Object(id='535598974980325386'), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+numberending'** member! We appreciate your stay and hope that we can help you!')
 	
-client.run(TOKEN)
+client.run(os.getenv('TOKEN'))
