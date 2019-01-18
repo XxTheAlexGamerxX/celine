05597ff1a76af9cf009954d17629f987895f4ead
XxTheAlexGamerxX <45047557+XxTheAlexGamerxX@users.noreply.github.com>
2019-01-18 18:47:08 +0100

    Create celine.py

---
+++ celine.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from tinydb import TinyDB, Query
+from tinydb.operations import delete
+import random
+import time
+
+db=TinyDB('data_Celine.json')
+user=Query()
+
+client = commands.Bot(command_prefix = '+')
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+	await client.change_presence(game=discord.Game(name='out for requests!', type=3))
+	print('Startup complete!')
+	
+@client.event
+async def on_member_join(member):
+	
+	membercount = int(member.server.member_count)
+	
+	if membercount % 10 == 1:
+		numberending = 'st'
+		
+	if membercount % 10 == 2:
+		numberending = 'nd'
+		
+	if membercount % 10 not 1 and membercount % 10 not 2:
+		numberending = 'th'
+	
+	await client.send_message(discord.Object(id='535598974980325386'), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+numberending'** member! We appreciate your stay and hope that we can help you!')
+	
+client.run(os.getenv('TOKEN'))
