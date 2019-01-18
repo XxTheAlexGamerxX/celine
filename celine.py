9a9000c03f68df1a881d79596af3dfb590b59da7
XxTheAlexGamerxX <45047557+XxTheAlexGamerxX@users.noreply.github.com>
2019-01-18 22:01:30 +0100

    Update celine.py

--- celine.py
+++ celine.py
@@ -4,6 +4,8 @@ import os
 import random
 import time
 
+TOKEN='NTM1NTk2NzA1NzQ4NDg0MDk2.DyPOcg.IXgT3MwWs0lrUF55yD0Z_vjzxL0'
+
 client = commands.Bot(command_prefix = '+')
 client.remove_command('help')
 
@@ -28,4 +30,4 @@ async def on_member_join(member):
 	
 	await client.send_message(discord.Object(id='535598974980325386'), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+numberending'** member! We appreciate your stay and hope that we can help you!')
 	
-client.run(os.getenv('Token'))
+client.run(TOKEN)
