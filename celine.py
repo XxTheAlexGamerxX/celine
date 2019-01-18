65644663de742a6acd66c68111b7d776c9979c94
XxTheAlexGamerxX <45047557+XxTheAlexGamerxX@users.noreply.github.com>
2019-01-18 21:46:36 +0100

    Update celine.py

--- celine.py
+++ celine.py
@@ -27,4 +27,4 @@ async def on_member_join(member):
 	
 	await client.send_message(discord.Object(id='535598974980325386'), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+numberending'** member! We appreciate your stay and hope that we can help you!')
 	
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('Token'))
