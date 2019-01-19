6b1644986da0d92a87cddd094d3475aa6ca48643
XxTheAlexGamerxX <45047557+XxTheAlexGamerxX@users.noreply.github.com>
2019-01-19 20:31:22 +0100

    Update celine.py

--- celine.py
+++ celine.py
@@ -14,27 +14,19 @@ async def on_ready():
 	
 @client.event
 async def on_member_join(member):
-	
 	membercount = int(member.server.member_count)
-	
 	numberending = ''
-	
 	if membercount % 10 == 1:
 		numberending = 'st'
-		
 	if membercount % 10 == 2:
 		numberending = 'nd'
-		
 	if not membercount % 10 == 1 and not membercount % 10 == 2:
 		numberending = 'th'
-	
-	await client.send_message(discord.Object(id='535598974980325386'), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+''+str(numberending)+'** member! We appreciate your stay and hope that we can help you!')
-	
-	await client.process_commands(member)
+	await client.send_message(discord.Object(id=535598974980325386), 'Welcome <@'+str(member.id)+'> to the **Coding Helpcenter**. You are our**'+str(membercount)+''+str(numberending)+'** member! We appreciate your stay and hope that we can help you!')
 	
 @client.command(pass_context=True)
 async def ican(ctx, userrole):
-        availableroles = ['Java', 'C/C++/C#', 'Python', 'Visual Basic .NET', 'Perl', 'Ruby', 'Swift', 'HTML', 'PHP', 'CSS', 'Javascript']
+	availableroles = ['Java', 'C/C++/C#', 'Python', 'Visual Basic .NET', 'Perl', 'Ruby', 'Swift', 'HTML', 'PHP', 'CSS', 'Javascript']
 	if ctx.message.channel == discord.Object(id=533784202039132170):
 		if userrole in ctx.message.author.roles:
 			await client.say('Whoops... Looks as if ou already have that role ^0^')
@@ -43,8 +35,7 @@ async def ican(ctx, userrole):
 		if userrole in availableroles and not role in ctx.message.author.roles:
 			await client.add_roles(ctx.message.author, userrole)
 			await client.say('**'+ctx.message.author.name+'**, successfully gave you the role `'+role+'`!')
-		
-	else:
-		return
+		else:
+			return
 					 
 client.run(os.getenv('Token'))
